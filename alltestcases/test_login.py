0c20cc8d153159b60d9d04d468bccbd75b269a21
xinlinger <1041281256@qq.com>
2018-05-27 17:33:08 +0800

    alltestcases

--- alltestcases/test_login.py
+++ alltestcases/test_login.py
@@ -46,7 +46,7 @@
 
 
 
-
+# 截屏优化
 
 # import unittest
 
@@ -108,6 +108,60 @@
 
 
 
+#使用ddt
+
+# import unittest
+
+# from ddt import ddt,data,unpack,file_data
+# from common.create_driver import getDriver
+# from common.manage_dir import getPngfileName
+# import os
+# import time
+
+
+# @ddt
+# class UserActionTest(unittest.TestCase):
+#     driver =  getDriver()
+
+#     def setUp(self):
+#         self.driver.maximize_window()
+#         self.driver.get('http://118.31.19.120:3000/')
+
+#     def tearDown(self):
+#         print(getPngfileName())
+#         self.driver.save_screenshot(getPngfileName())
+#         self.driver.delete_all_cookies()
+
+#     @classmethod
+#     def setUpClass(cls):
+#         pass   
+
+#     @classmethod
+#     def tearDownClass(cls):
+#         cls.driver.quit()
+
+
+#     @data(["testuser3","123456"],["testuser4",""])
+#     def test_login(self,userinfo):
+#         self.driver.find_element_by_link_text('登录').click()
+#         self.driver.find_element_by_id('name').send_keys(userinfo[0])
+#         self.driver.find_element_by_id('pass').send_keys(userinfo[1])
+#         self.driver.find_element_by_id('pass').submit()
+#         okurl = self.driver.current_url
+
+#         self.assertEqual(okurl,'http://118.31.19.120:3000/')
+
+#         loginName = self.driver.find_element_by_css_selector('#sidebar > div:nth-child(1) > div.inner > div > div > span.user_name > a').text
+
+#         self.assertEqual(loginName,'testuser3')
+
+
+# if __name__ == "__main__":
+#     unittest.main()
+
+
+
+#引入CSV数据文件
 
 import unittest
 
@@ -116,6 +170,16 @@ from common.create_driver import getDriver
 from common.manage_dir import getPngfileName
 import os
 import time
+import csv
+
+def get_data_csv(filename):
+    users = []
+    with open(filename,'r') as f:
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            users.append(row)
+    return users
 
 
 @ddt
@@ -140,20 +204,30 @@ class UserActionTest(unittest.TestCase):
         cls.driver.quit()
 
 
-    @data(["testuser3","123456"],["testuser4",""])
-    def test_login(self,userinfo):
+    @data(*get_data_csv('./testdata/userlogin.csv'))
+    @unpack
+    def test_login(self,username,password,excpetStatus,checkpoint):
+
         self.driver.find_element_by_link_text('登录').click()
-        self.driver.find_element_by_id('name').send_keys(userinfo[0])
-        self.driver.find_element_by_id('pass').send_keys(userinfo[1])
+        self.driver.find_element_by_id('name').send_keys(username)
+        self.driver.find_element_by_id('pass').send_keys(password)
         self.driver.find_element_by_id('pass').submit()
         okurl = self.driver.current_url
 
         self.assertEqual(okurl,'http://118.31.19.120:3000/')
 
         loginName = self.driver.find_element_by_css_selector('#sidebar > div:nth-child(1) > div.inner > div > div > span.user_name > a').text
-
         self.assertEqual(loginName,'testuser3')
 
+    # def test_register(self):
+    #     self.driver.find_element_by_link_text('注册').click()
+    #     self.driver.get('http://118.31.19.120:3000/signup')
+    #     self.driver.find_element_by_id('loginname').send_keys('testuser3')
+    #     self.driver.find_element_by_id('pass').send_keys('123456')
+    #     self.driver.find_element_by_id('re_pass').send_keys("123456")
+    #     self.driver.find_element_by_id('email').send_keys('123456@123.com')
+    #     self.driver.find_element_by_id('pass').submit()
+        
 
 if __name__ == "__main__":
     unittest.main()
